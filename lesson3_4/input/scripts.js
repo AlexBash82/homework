c17ad223c59c84ba4756de0d4c0dacaee66a2306
AlexBash82 <kometa.4m@gmail.com>
2022-11-01 23:36:18 +0500

    input have done

--- lesson3_4/input/scripts.js
+++ lesson3_4/input/scripts.js
@@ -4,17 +4,23 @@ const searchInput = document.querySelector(".js-input");
 
 loadBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-  const searchValue = searchInput.value.trim()
+  const searchValue = searchInput.value
   try {
     if(searchValue === ''){
+      throw new Error('Строка не должна быть пустой!', searchValue)              
+    } else if(searchValue > 10) {
+      throw new Error('это больше 10 !!!', searchValue)  
+    } else if(searchValue < 5){
+      throw new Error('это меньше 5 !!!', searchValue)
+    } else if(searchValue > 4 && searchValue < 11) {
       resultsContainer.innerHTML = (`<div class="response-container">                
-      <p> Строка не должна быть пустой<p> 
-    </div>`)
+      <p><span>Все верно!</span><p>`)
+    } else {
+    throw new Error('это не число!', searchValue)
     }
-    throw new Error(searchValue)
   } catch (error) {
     resultsContainer.innerHTML = (`<div class="response-container">                
-      <p> Строка  <span>${searchValue}</span><p> 
+      <p><span>ERROR: ${searchValue} ${error.message}</span><p> 
     </div>`)
   }
 
